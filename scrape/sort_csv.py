f0c4133a98a8fb5bf3ec3e8e7ebcf3c8e01edb5b
Anne-Ruth Meijer <anneruth02@gmail.com>
2018-06-07 15:28:52 +0200

    Made the code better and added comments. Also made a csv fro 2017

--- scrape/sort_csv.py
+++ scrape/sort_csv.py
@@ -1,34 +1,48 @@
 import pandas as pd
 import csv
 
-with open('training.csv', 'rb') as csvfile:
+# The output file.
+OUTPUT = "2017_1078_Solarpanel.csv"
+
+# The input file
+INPUT = "temp.csv"
+
+# The first row in the output file.
+COLUMNS = ("Date,Generated,Efficiency,Number_of_panels,Max_power,System_size,"
+	+ "Number_of_inverters,Inverter_size,Postcode,Install_date,Tilt\n")
+
+# Opens the input file to read the first column with the dates.
+with open(INPUT, "rb") as csvfile:
 	df = pd.read_csv(csvfile)
 	saved_column = df.Date 
 	output = []
 	for item in saved_column:
 		output.append(item)
+	# Sorts the dates.
 	output.sort()
 
-COLUMNS = "Date,Generated,Efficiency,Number_of_panels,Max_power,System_size,Panel_brand,Orientation,Number_of_inverters,Inverter_brand,Inverter_size,Postcode,Install_date,Shading,Tilt\n"
-
-output_file = open('sorted_training.csv','a')
+output_file = open(OUTPUT,"a")
 output_file.write(COLUMNS)
+
+# Creates a new output file with sorted dates.
 for item in output:
-	file = open('training.csv', "rU")
-	reader = csv.reader(file, delimiter=',')
+	file = open(INPUT, "rU")
+	reader = csv.reader(file, delimiter=",")
 	firstrow = True
+
 	for row in reader:
 		if firstrow:
 			firstrow = False
 		else:
 			if int(row[0]) == item:
 				firstvalue = True
+
 				for value in row:
 					if firstvalue:
 						firstvalue = False
 						output_file.write(str(value))
 					else:
-						output_file.write(","+str(value))
+						output_file.write("," + str(value))
 				output_file.write("\n")
 output_file.close()
 
