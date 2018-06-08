5b781c0c4168c03e9832848e0a5f523ad78b8d77
basvdbrink1998 <basvdbrink1998@gmail.com>
2018-06-08 11:50:02 +0200

    cleaned code and added a year of data

--- Weather/Weather_test.py
+++ Weather/Weather_test.py
@@ -8,7 +8,8 @@ import numpy as np
 import pandas as pd
 
 
-SECRET_KEY = "4026e043d76e5963c2c57ab535353197"
+#SECRET_KEY = "4026e043d76e5963c2c57ab535353197"
+SECRET_KEY = "59e1462995d5ab68f2c1f29478f98081"
 FACTORS = ["time","cloudCover","sunriseTime","sunsetTime","temperatureHigh","temperatureLow","temperatureMin","temperatureMax","visibility"]
 
 
@@ -38,9 +39,9 @@ def get_features(longitude,latitude,date):
 def get_data(longitude,latitude,year):
 #Gets the weather of a calendar year
     if calendar.isleap(year):
-        size = (364,len(FACTORS))
+        size = (367,len(FACTORS))
     else:
-        size = (365,len(FACTORS))
+        size = (367,len(FACTORS))
     data = np.zeros(size)
     nday = 0
     for month in range(1,13):
@@ -50,7 +51,7 @@ def get_data(longitude,latitude,year):
         below that to reduce the amount of API requests
         '''
         for day in range(1,calendar.monthrange(year,month)[1]+1):
-        #for day in range(1,2):
+        #for day in range(1,11):
             date = datetime.date(year,month,day)
             features = get_features(longitude,latitude,date)["daily"]["data"][0]
             toAdd = []
@@ -63,31 +64,30 @@ def get_data(longitude,latitude,year):
             nday += 1
     return data
 
-def make_database(file_name,year):
-    #Makes a weather database and saves it
-    result = get_data(longitude,latitude,year)
-    res = pd.DataFrame(result,columns=FACTORS)
-    res.to_csv(file_name)
-
-def normalize(colomn):
+def normalize(col):
     #normalizes a colomn
     return col - np.mean(col)
 
 def normalize_data_base(db):
     #normalizes a database
     for col in db:
-        db[column] = normalize(db[col])
+        db[col] = normalize(db[col])
+    return db
+
+def make_database(longitude,latitude,file_name,year):
+    #Makes a weather database and saves it
+    result = get_data(longitude,latitude,year)
+    res = pd.DataFrame(result,columns=FACTORS)
+    db = normalize_data_base(res)
+    db.to_csv(file_name)
 
 def _main_(postal_code,year,file_name):
     [longitude, latitude] = get_LL("1078",read_file())
-    db = []
     if type(year) == int:
-        db.append(get_data(longitude,latitude,year))
+        db = make_database(longitude,latitude,str(postal_code) + file_name + str(year) + ".csv",year)
     elif type(year) == list:
-        for i in year:
-            print('year:',i)
-            db.append(get_data(longitude,latitude,i))
-    db = normalize_data_base(db)
-    res.to_csv(file_name)
+        for i in range(0,len(year)):
+            print('year:',year[i])
+            db = make_database(longitude,latitude,str(postal_code) + file_name + str(year[i]) + ".csv",year[i])
 
-_main_(2152,[2016,2017],"Weather2017.csv")
+_main_(1078,[2016,2017],"Weather")
