8d7b9b70c95a028452fa809d58480f9e91c183d3
CaitSmithUvA <cait.d.smith@gmail.com>
2018-06-13 12:26:49 +0200

    Neualnet with five layers

--- NN/NN.py
+++ NN/NN.py
@@ -22,36 +22,48 @@ x_test =  W[train_size:,:]
 y_train = results[:train_size]
 y_test = results[train_size:]
 
-def nn_model(x_data, input_dim, layer_dim):
-    Weights_1 = tf.Variable(tf.random_uniform([input_dim, layer_dim]))
-    bias_1 = tf.Variable(tf.zeros([layer_dim]))
+def nn_model(x_data, input_dim):
+    Weights_1 = tf.Variable(tf.random_uniform([input_dim, 16]))
+    bias_1 = tf.Variable(tf.zeros([16]))
 
     layer_1 = tf.add(tf.matmul(x_data, Weights_1), bias_1)
     layer_1 = tf.nn.relu(layer_1)
 
-    Weights_2 = tf.Variable(tf.random_uniform([layer_dim, layer_dim]))
-    bias_2 = tf.Variable(tf.zeros([layer_dim]))
+    Weights_2 = tf.Variable(tf.random_uniform([16, 8]))
+    bias_2 = tf.Variable(tf.zeros([8]))
 
     layer_2 = tf.add(tf.matmul(layer_1, Weights_2), bias_2)
     layer_2 = tf.nn.relu(layer_2)
 
-    Weights_output = tf.Variable(tf.random_uniform([layer_dim, 1])) #dtype=tf.float32
+    Weights_3 = tf.Variable(tf.random_uniform([8, 8]))
+    bias_3 = tf.Variable(tf.zeros([8]))
+
+    layer_3 = tf.add(tf.matmul(layer_2, Weights_3), bias_3)
+    layer_3 = tf.nn.relu(layer_3)
+
+    Weights_4 = tf.Variable(tf.random_uniform([8, 4]))
+    bias_4 = tf.Variable(tf.zeros([4]))
+
+    layer_4 = tf.add(tf.matmul(layer_3, Weights_4), bias_4)
+    layer_4 = tf.nn.relu(layer_4)
+
+    Weights_output = tf.Variable(tf.random_uniform([4, 1])) #dtype=tf.float32
     bias_output = tf.Variable(tf.zeros([1]))
 
-    output = tf.add(tf.matmul(layer_2, Weights_output), bias_output)
+    output = tf.add(tf.matmul(layer_4, Weights_output), bias_output)
 
     return output
 
 xs = tf.placeholder("float")
 ys = tf.placeholder("float")
 
-output = nn_model(xs,3, 16)
+output = nn_model(xs,3)
 
 # our mean squared error cost function
 cost = tf.reduce_mean(tf.square(output-ys))
 
 # Gradinent Descent optimiztion just discussed above for updating weights and biases
-train = tf.train.GradientDescentOptimizer(0.001).minimize(cost)
+train = tf.train.GradientDescentOptimizer(0.75).minimize(cost)
 
 c_t = []
 c_test = []
@@ -72,7 +84,7 @@ with tf.Session() as sess:
         # print each individual cost
         c_t.append(sess.run(cost, feed_dict={xs:x_train.reshape(-1,3), ys:y_train}))
         c_test.append(sess.run(cost, feed_dict={xs:x_test.reshape(-1, 3), ys:y_test}))
-        #print(i, "%, Cost: ", c_t[i])
+        print(i, "%, Cost: ", c_t[i])
 
     #predict output of test data after training
     predict = sess.run(output, feed_dict={xs:x_test.reshape(-1,3)})
