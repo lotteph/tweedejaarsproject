322f1e4cd9a629f4e1d33feb795b277ba8a2c0e9
Anne-Ruth Meijer <ruth@wcw-stud-145-109-60-241.wireless.uva.nl>
2018-06-18 13:00:42 +0200

    cleaned scrape.py

--- data/scrape.py
+++ data/scrape.py
@@ -4,29 +4,19 @@ from datetime import datetime
 import time
 import calendar
 
-# The month and year where the scraping must begin.
-BEGIN_MONTH = 1
-BEGIN_YEAR = 2018
-
-# The month and year where the scraping must end.
-END_MONTH = 1
-END_YEAR = 2019
-
-# https://pvoutput.org/list.jsp?df=20180501&dt=20180531&id=18865&sid=16711&t=m&gs=0&v=0
-
 # The output file.
-FILE = "2018_2201_S.csv"
+FILE = "temp.csv"
 
 # The first row in the output file.
 COLUMNS = ("Date,Generated,Efficiency,Number_of_panels,Max_power,System_size,"
     + "Number_of_inverters,Inverter_size,Postcode,Install_date,Tilt\n")
 
-def panel_info(begin_date, end_date):
+def panel_info(begin_date, end_date, id, sid):
     ''' Function to scrape the information from the solarpanel used
         to generate energy.
     '''
     url = ("https://pvoutput.org/list.jsp?df=" + begin_date + "&dt=" + end_date
-        + "&id=18865&sid=16711&t=m&gs=0&v=0")
+        + "&id=" + id + "&sid=" + sid + "&t=m&gs=0&v=0")
     page = urlopen(url)
     soup = BeautifulSoup(page, "html.parser")
     panel_info = soup.find("a", attrs={"class": "system1"})
@@ -70,7 +60,6 @@ def add_data(data, panel, file):
                 dt = datetime.strptime(text, "%d/%m/%y")
                 unix = datetime(dt.year, dt.month, dt.day, 0, 0)
                 unix = time.mktime(unix.timetuple())
-
                 file.write(str(unix))
 
             # Strips the energy values from their unit of measurements.
@@ -89,65 +78,62 @@ def add_data(data, panel, file):
                 file.write("\n")
             counter += 1
 
-def retrieve_data(begin_date, end_date, panel, file, second):
+def retrieve_data(begin_date, end_date, panel, file, second, id, sid):
     ''' Scrapes energy data from pvoutput.org per month given the begin and end
-        date. When the month has 31 days the data is located on two pages. These
-        both gets scraped in this case.
+        date. When the month has 31 days the data is located on two pages. In
+        that case both pages get scraped.
     '''
-    url = ("https://pvoutput.org/list.jsp?df=" + begin_date + "&dt="
-        + end_date + "&id=18865&sid=16711&t=m&gs=0&v=0")
-    sec_url = ("https://pvoutput.org/list.jsp?p=1&id=18865&sid=16711&gs=0&df="
-        + begin_date + "&dt=" + end_date +"&v=0&o=date&d=desc")
+    url = ("https://pvoutput.org/list.jsp?df=" + begin_date + "&dt=" + end_date
+        + "&id=" + id + "&sid=" + sid + "&t=m&gs=0&v=0")
+    sec_url = ("https://pvoutput.org/list.jsp?p=1&id=" + id + "&sid=" + sid +
+    "&gs=0&df=" + begin_date + "&dt=" + end_date +"&v=0&o=date&d=desc")
 
     page = urlopen(url)
     soup = BeautifulSoup(page, "html.parser")
     data = soup.findAll("tr", attrs={"class": ["e2", "o2"]})
     add_data(data, panel, file)
 
-    # If there is a second page scrape that site to.
+    # If there is a second page scrape that page to.
     if second == True:
         sec_page = urlopen(sec_url)
         sec_soup = BeautifulSoup(sec_page, "html.parser")
         data = sec_soup.findAll("tr", attrs={"class": ["e2", "o2"]})
         add_data(data, panel, file)
 
-def main():
+def main(m, y, end_m, end_y, id, sid):
     ''' Runs all the functions to create a csv file with all the
         important data from the solarpanels.
     '''
-    y = BEGIN_YEAR
-    m = BEGIN_MONTH
-
     if m < 10:
-        begin = str(y)+"0"+str(m)+"01"
+        begin = str(y) + "0" + str(m) + "01"
         end_d = calendar.monthrange(y, m)
-        end = str(y)+"0"+str(m)+str(end_d[1])
+        end = str(y) + "0" + str(m) + str(end_d[1])
     else:
-        begin = str(y)+str(m)+"01"
+        begin = str(y) + str(m) + "01"
         end_d = calendar.monthrange(y, m)
-        end = str(y)+str(m)+str(end_d[1])
+        end = str(y) + str(m) + str(end_d[1])
 
-    panel = panel_info(begin, end)
+    panel = panel_info(begin, end, id, sid)
     csv_file = open(FILE,"a")
     csv_file.write(COLUMNS)
 
     # Retrieves data for each month
-    while y != END_YEAR or m != END_MONTH:
+    while y != end_y or m != end_m:
         end_d = calendar.monthrange(y, m)
         if m < 10:
-            begin = str(y)+"0"+str(m)+"01"
-            end = str(y)+"0"+str(m)+str(end_d[1])
+            begin = str(y) + "0" + str(m) + "01"
+            end = str(y) + "0" + str(m) + str(end_d[1])
         else:
-            begin = str(y)+str(m)+"01"
-            end = str(y)+str(m)+str(end_d[1])
+            begin = str(y) + str(m) + "01"
+            end = str(y) + str(m) + str(end_d[1])
 
         if end_d[1] > 30:
             time.sleep(.5)
             # Change to False when there are no second pages.
-            retrieve_data(begin, end, panel, csv_file, True)
+            retrieve_data(begin, end, panel, csv_file, True, id, sid)
         else:
             time.sleep(.5)
-            retrieve_data(begin, end, panel, csv_file, False)
+            retrieve_data(begin, end, panel, csv_file, False, id, sid)
 
         if m != 12:
             m += 1
@@ -157,4 +143,4 @@ def main():
     csv_file.close()
 
 if __name__ == "__main__":
-    main()
+    main(1, 2013, 1, 2014, "13448", "13242")
