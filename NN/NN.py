647a6c666cf796687f2a8bd109140fddbc84c710
CaitSmithUvA <cait.d.smith@gmail.com>
2018-06-18 11:33:08 +0200

    NNpy ready to be tweaked and can be saved

--- NN/NN.py
+++ NN/NN.py
@@ -5,24 +5,8 @@ import tensorflow as tf
 import sys
 from sklearn.utils import shuffle
 
-# W = np.transpose(pd.read_csv("1078Weather2016.csv").values)
-# W = np.transpose(W)[:,1:10]
-# SP = pd.read_csv("2016_1078_Solarpanel.csv")
-# results = np.array(SP["Generated"])
-#
-# W, results = shuffle(W,results, random_state=0)
-#
-# train_size = int(len(results)*(2/3))
-#
-# # x is input weather data
-# x_train = W[:train_size,:]
-# x_test =  W[train_size:,:]
-#
-# # y is solar panel output data
-# y_train = results[:train_size]
-# y_test = results[train_size:]
 STEP_SIZE = 100
-LEARNING_RATE = 0.1
+LEARNING_RATE = 0.00005
 
 def make_csv(solar, weather):
     new = weather
@@ -35,23 +19,23 @@ def make_csv(solar, weather):
     new["tilt"] = solar["Tilt"]
     return np.array(new)
 
-years = ["2016","2017"]
-
-W = pd.read_csv("1078Weather" + years[0] + ".csv")
-SP = pd.read_csv(years[0] + "_1078_Solarpanel.csv")
+years = ["2013","2014","2015","2016","2017","2018"]
+postal_code = "7559"
+W = pd.read_csv("../NN/"+postal_code+ "_" + years[0] + "_W.csv")
+SP = pd.read_csv("../NN/"+postal_code+ "_" + years[0] + "_S.csv")
 results = np.array(SP["Generated"])
 for year in range(1,len(years)):
-    W2 = pd.read_csv("1078Weather" + years[year] + ".csv")
+    W2 = pd.read_csv("../NN/"+postal_code+ "_" + years[year] + "_W.csv")
     W = pd.DataFrame.append(W,W2)
-    SP2 = pd.read_csv(years[year] + "_1078_Solarpanel.csv")
+    SP2 = pd.read_csv("../NN/"+postal_code+ "_" + years[year] + "_S.csv")
     SP = pd.DataFrame.append(SP,SP2)
 results = np.array(SP["Generated"])
+#print(results)
+sizes = 1
+#print(sizes)
 
-W = make_csv(pd.DataFrame(SP), pd.DataFrame(W))
-
-W, results = shuffle(W,results, random_state=0)
-
-train_size = int(len(results)*(2/3))
+W = (W.values)
+train_size = len(results)-365
 
 x_train = W[:train_size,:]
 x_test =  W[train_size:,:]
@@ -59,31 +43,31 @@ y_train = results[:train_size]
 y_test = results[train_size:]
 
 def nn_model(x_data, input_dim):
-    Weights_1 = tf.Variable(tf.random_uniform([input_dim, 50]))
-    bias_1 = tf.Variable(tf.zeros([50]))
+    Weights_1 = tf.Variable(tf.random_uniform([input_dim, 16]))
+    bias_1 = tf.Variable(tf.zeros([16]))
 
     layer_1 = tf.add(tf.matmul(x_data, Weights_1), bias_1)
     layer_1 = tf.nn.relu(layer_1)
 
-    # Weights_2 = tf.Variable(tf.random_uniform([16, 8]))
-    # bias_2 = tf.Variable(tf.zeros([8]))
-    #
-    # layer_2 = tf.add(tf.matmul(layer_1, Weights_2), bias_2)
-    # layer_2 = tf.nn.relu(layer_2)
+    Weights_2 = tf.Variable(tf.random_uniform([16, 8]))
+    bias_2 = tf.Variable(tf.zeros([8]))
 
-    # Weights_3 = tf.Variable(tf.random_uniform([16, 8]))
-    # bias_3 = tf.Variable(tf.zeros([8]))
-    #
-    # layer_3 = tf.add(tf.matmul(layer_1, Weights_3), bias_3)
-    # layer_3 = tf.nn.relu(layer_3)
+    layer_2 = tf.add(tf.matmul(layer_1, Weights_2), bias_2)
+    layer_2 = tf.nn.relu(layer_2)
 
-    Weights_4 = tf.Variable(tf.random_uniform([50, 20]))
-    bias_4 = tf.Variable(tf.zeros([20]))
+    Weights_3 = tf.Variable(tf.random_uniform([8, 8]))
+    bias_3 = tf.Variable(tf.zeros([8]))
 
-    layer_4 = tf.add(tf.matmul(layer_1, Weights_4), bias_4)
+    layer_3 = tf.add(tf.matmul(layer_2, Weights_3), bias_3)
+    layer_3 = tf.nn.relu(layer_3)
+
+    Weights_4 = tf.Variable(tf.random_uniform([8, 4]))
+    bias_4 = tf.Variable(tf.zeros(4))
+
+    layer_4 = tf.add(tf.matmul(layer_3, Weights_4), bias_4)
     layer_4 = tf.nn.relu(layer_4)
 
-    Weights_output = tf.Variable(tf.random_uniform([20, 1])) #dtype=tf.float32
+    Weights_output = tf.Variable(tf.random_uniform([4, 1])) #dtype=tf.float32
     bias_output = tf.Variable(tf.zeros([1]))
 
     model = tf.add(tf.matmul(layer_4, Weights_output), bias_output)
@@ -93,7 +77,7 @@ def nn_model(x_data, input_dim):
 xs = tf.placeholder("float")
 ys = tf.placeholder("float")
 
-model = nn_model(xs,3)
+model = nn_model(xs,1)
 
 # our mean squared error cost function
 cost = tf.reduce_mean(tf.square(model-ys))
@@ -109,22 +93,35 @@ with tf.Session() as sess:
 
     sess.run(tf.global_variables_initializer())
     saver = tf.train.Saver()
-    #saver.restore(sess, "dataset.csv")
+    #saver.restore(sess, '/home/cait/tweedejaarsproject/NN/dataset.ckpt')
 
     # run with each sample for cost and train
     for i in range(STEP_SIZE):
-        for j in range(x_train.shape[0]):
-            dicti = {xs:x_train[j:].reshape(-1,3), ys: y_train[j]} #size -1 for unspecified
-            sess.run([cost, train], feed_dict = dicti)
+       for j in range(x_train.shape[0]):
+           dicti = {xs:x_train[j:].reshape(-1,sizes), ys: y_train[j]} #size -1 for unspecified
+           sess.run([cost, train], feed_dict = dicti)
 
         # print each individual cost
-        c_t.append(sess.run(cost, feed_dict={xs:x_train.reshape(-1,3), ys:y_train}))
-        if i%10 == 0:
-            print("Step:", i, ", Cost:", c_t[i])
+       c_t.append(sess.run(cost, feed_dict={xs:x_train.reshape(-1,sizes), ys:y_train}))
+       if i%10 == 0:
+           print("Step:", i, ", Cost:", c_t[i])
 
     #predict output of test data after training
-    predict = sess.run(model, feed_dict={xs:x_test.reshape(-1,3)})
+    predict = sess.run(model, feed_dict={xs:x_test.reshape(-1,sizes)})
+    print(len(predict))
+    print(len(x_test.reshape(-1,sizes)))
     AV = sum(np.square(np.mean(predict))-y_test)
 
     print("Error: ", predict[-1][0])
     #print("OMG HET LEEFT")
+
+    if input('Save model ? [Y/N]').upper() == 'Y':
+       saver.save(sess,'/home/cait/tweedejaarsproject/NN/dataset.ckpt')
+       print('Model Saved')
+
+# Later, launch the model, use the saver to restore variables from disk, and
+# do some work with the model.
+  # Restore variables from disk.
+    # saver.restore(sess, "/home/cait/tweedejaarsproject/NN/dataset.ckpt")
+    # print("Model restored.")
+  # Check the values of the variables
