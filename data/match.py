064f06053197669b9cce4d5b2bfa32a142c8adf0
Anne-Ruth Meijer <ruth@wcw-stud-145-109-30-200.wireless.uva.nl>
2018-06-19 13:10:27 +0200

    added the old scrape and match files

---
+++ data/match.py
@@ -0,0 +1,103 @@
+import csv
+import calendar
+import numpy as np
+import pandas as pd
+
+def add_month(weather_file, year):
+    months = []
+
+    for i in range(1,13):
+        days = calendar.monthrange(year, i)
+        for j in range(days[1]):
+            months.append(i)
+
+    months = np.array(months)
+    months = pd.DataFrame(months)
+    weather = pd.read_csv(weather_file)
+    del weather["Unnamed: 0"]
+    weather["month"] = months
+    weather.to_csv(weather_file)
+
+
+def make_solarcsv(solar_file, postal_code, year):
+    new_file_name = str(postal_code) + "_" + str(year) + "_S.csv"
+    new_file = open(new_file_name, "w")
+
+    with open(solar_file, "rU") as csvfile1:
+        solardata = csv.reader(csvfile1)
+
+        count = 0
+        for row in solardata:
+            if count == 0:
+                firstvalue = True
+                for value in row:
+                    if firstvalue == True:
+                        new_file.write(str(value))
+                        firstvalue = False
+                    else:
+                        new_file.write("," + str(value))
+                new_file.write("\n")
+
+            if row[1] != "0000" and count != 0:
+                firstvalue = True
+                for value in row:
+                    if firstvalue == True:
+                        new_file.write(str(value))
+                        firstvalue = False
+                    else:
+                        new_file.write("," + str(value))
+                new_file.write("\n")
+
+            count += 1
+
+    csvfile1.close()
+    new_file.close()
+    return(new_file_name)
+
+
+def make_weathercsv(solarcsv, weather_file, postal_code, year):
+    with open(weather_file, "rU") as csvfile2, open(solarcsv, "rU") as csvfile1:
+        weather = csv.reader(csvfile2)
+        solar = csv.reader(csvfile1)
+        new_file = open(str(postal_code) + "_" + str(year) + "_W.csv", "w")
+
+        count = 0
+
+        for row1 in solar:
+            file = open(weather_file, "rU")
+            weather = csv.reader(file, delimiter=",")
+
+            for row2 in weather:
+                if count == 0:
+                    firstvalue = True
+                    for value in row2:
+                        if firstvalue == True:
+                            new_file.write(str(value))
+                            firstvalue = False
+                        else:
+                            new_file.write("," + str(value))
+                    new_file.write("\n")
+
+                if row1[0] == row2[1] and count != 0:
+
+                    firstvalue = True
+                    for value in row2:
+                        if firstvalue == True:
+                            new_file.write(str(value))
+                            firstvalue = False
+                        else:
+                            new_file.write("," + str(value))
+                    new_file.write("\n")
+
+                    break;
+
+                count += 1
+
+        csvfile1.close()
+        csvfile2.close()
+        new_file.close()
+
+if __name__ == "__main__":
+    add_month("2013_2201_W.csv", 2013)
+    solarcsv = make_solarcsv("2013_2201_S.csv", "2201", "2013")
+    make_weathercsv(solarcsv, "2013_2201_W.csv", "2201", "2013")
