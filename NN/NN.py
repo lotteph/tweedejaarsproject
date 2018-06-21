0ad0ce5bead311f85ad8e3fd3b2c1398638748f1
Lotte Philippus <lotte.philippus@gmail.com>
2018-06-21 11:54:35 +0200

    made new neural network

--- NN/NN.py
+++ NN/NN.py
@@ -9,8 +9,8 @@ from sklearn.utils import shuffle
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-STEP_SIZE = 100
-LEARNING_RATE = 0.0005
+STEP_SIZE = 500
+LEARNING_RATE = 0.0001
 
 def make_csv(solar, weather):
     new = weather
@@ -24,7 +24,29 @@ def make_csv(solar, weather):
     return np.array(new)
 
 years = ["2013","2014","2015","2016","2017","2018"]
+postal_code = ["2134", "2201", "7325", "7559"]
+
+SP = False
+W = False
+for code in range(0,len(postal_code)):
+    for year in range(0,len(years)):
+        W2 = pd.read_csv("./data/"+postal_code[code]+ "_" + years[year] + "_W.csv")
+        if type(W) != type(False):
+            W = pd.DataFrame.append(W,W2)
+        else:
+            W = W2
+        SP2 = pd.read_csv("./data/"+postal_code[code]+ "_" + years[year] + "_S.csv")
+        if type(SP) != type(False):
+            SP = pd.DataFrame.append(SP,SP2)
+        else:
+            SP = SP2
+results = np.array(SP["Generated"])
+W["time"] = 0
+W = make_csv(SP, W)
+
+years = ["2013","2014","2015","2016","2017","2018"]
 postal_code = "7559"
+
 W = pd.read_csv("../NN/data/"+postal_code+ "_" + years[0] + "_W.csv")
 SP = pd.read_csv("../NN/data/"+postal_code+ "_" + years[0] + "_S.csv")
 results = np.array(SP["Generated"])
@@ -34,61 +56,16 @@ for year in range(1,len(years)):
     SP2 = pd.read_csv("../NN/data/"+postal_code+ "_" + years[year] + "_S.csv")
     SP = pd.DataFrame.append(SP,SP2)
 results = np.array(SP["Generated"])
-sizes = 13
-
 W = (W.values)
+
 train_size = len(results)-365
 
-x_train = W[:train_size,:]
-x_test =  W[train_size:,:]
-y_train = results[:train_size]
-y_test = results[train_size:]
+sizes = W.shape[1]
 
-def nn_model(x_data, input_dim):
-    Weights_1 = tf.Variable(tf.random_uniform([input_dim, 16]))
-    bias_1 = tf.Variable(tf.zeros([16]))
-
-    layer_1 = tf.add(tf.matmul(x_data, Weights_1), bias_1)
-    layer_1 = tf.nn.relu(layer_1)
-
-    Weights_2 = tf.Variable(tf.random_uniform([16, 8]))
-    bias_2 = tf.Variable(tf.zeros([8]))
-
-    layer_2 = tf.add(tf.matmul(layer_1, Weights_2), bias_2)
-    layer_2 = tf.nn.relu(layer_2)
-
-    Weights_3 = tf.Variable(tf.random_uniform([8, 8]))
-    bias_3 = tf.Variable(tf.zeros([8]))
-
-    layer_3 = tf.add(tf.matmul(layer_2, Weights_3), bias_3)
-    layer_3 = tf.nn.relu(layer_3)
-
-    Weights_4 = tf.Variable(tf.random_uniform([8, 4]))
-    bias_4 = tf.Variable(tf.zeros(4))
-
-    layer_4 = tf.add(tf.matmul(layer_3, Weights_4), bias_4)
-    layer_4 = tf.nn.relu(layer_4)
-
-    Weights_output = tf.Variable(tf.random_uniform([4, 1])) #dtype=tf.float32
-    bias_output = tf.Variable(tf.zeros([1]))
-
-    model = tf.add(tf.matmul(layer_4, Weights_output), bias_output)
-
-    return model
-
-xs = tf.placeholder("float")
-ys = tf.placeholder("float")
-
-model = nn_model(xs, sizes)
-
-# Mean squared error cost function
-cost = tf.reduce_mean(tf.square(model-ys))
-
-# Gradient Descent optimiztion just discussed above for updating weights and biases
-#train = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(cost)
-#train = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cost)
-#train = tf.train.RMSPropOptimizer(LEARNING_RATE).minimize(cost)
-train = tf.train.MomentumOptimizer(LEARNING_RATE, momentum=0.0001).minimize(cost)
+x_train = W[:train_size,:]#.transpose()
+x_test =  W[train_size:,:]#.transpose()
+y_train = results[:train_size]#.transpose()
+y_test = results[train_size:]#.transpose()
 
 c_t = []
 
@@ -103,7 +80,7 @@ with tf.Session() as sess:
     # run with each sample for cost and train
     for i in range(STEP_SIZE):
         for j in range(x_train.shape[0]):
-            dicti = {xs:x_train[j:].reshape(-1,sizes), ys: y_train[j]} #size -1 for unspecified
+            dicti = {xs:x_train[j:].reshape(-1, sizes), ys: y_train[j]} #size -1 for unspecified
             sess.run([cost, train], feed_dict = dicti)
 
         # print each individual cost
@@ -116,7 +93,10 @@ with tf.Session() as sess:
 
     print("Error: ", predict[-1][0])
 
-    if input('Save model ? [Y/N]').upper() == 'Y':
+    plt.plot(predict)
+    plt.show()
+
+    if input('Save model ? [Y/N] ').upper() == 'Y':
         new_model_name = input('Please name the file: ')
         saver.save(sess, './models/' + new_model_name + '.ckpt')
         print('Model Saved')
---
+++ NN/NN_test.py
@@ -0,0 +1,174 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import sys
+from sklearn.utils import shuffle
+
+from tensorflow.contrib import learn
+from sklearn.pipeline import Pipeline
+from sklearn import datasets, linear_model
+from sklearn import cross_validation
+from sklearn.preprocessing import MinMaxScaler
+import scipy.ndimage
+
+# Disables the warnings
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+data = dict()
+data["7325"] = range(2013,2019)
+data["7559"] = range(2013,2019)
+data["2134"] = range(2013,2019)
+data["2201"] = range(2013,2019)
+data["6591"] = range(2015,2019)
+data["3481"] = range(2015,2019)
+postal_codes = ["7325","7559","2134","2201","6591","3481"]
+SP = False
+W = False
+print(data.keys())
+for code in postal_codes:
+    for year in data[code]:
+        W2 = pd.read_csv("../data/"+str(code)+ "_" + str(year) + "_W.csv")
+        if type(W) != type(False):
+            W = pd.DataFrame.append(W,W2)
+        else:
+            W = W2
+        SP2 = pd.read_csv("../data/"+ str(code) + "_" + str(year) + "_S.csv")
+        if type(SP) != type(False):
+            SP = pd.DataFrame.append(SP,SP2)
+        else:
+            SP = SP2
+results = np.array(SP["Generated"]/SP["Number_of_panels"]/SP["Max_power"])
+W["time"] = 0
+W = make_csv(SP,W)
+
+train_size = len(results)-365*3
+
+X_train = W[:train_size,:]
+X_test =  W[train_size:,:]
+Y_train = results[:train_size]
+Y_test = results[train_size:]
+
+offset = SP["Number_of_panels"].values[train_size:]*SP["Max_power"].values[train_size:]
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
+# Y_train = scaler.fit_transform(Y_train)
+# Y_test = scaler.fit_transform(Y_test)
+
+total_len = X_train.shape[0]
+
+# Parameters
+learning_rate = 0.001
+training_epochs = 2000
+batch_size = 32
+display_step = 1
+# Network Parameters
+n_hidden_1 = 32 # 1st layer number of features
+n_hidden_2 = 200 # 2nd layer number of features
+n_hidden_3 = 200
+n_hidden_4 = 256
+n_input = X_train.shape[1]
+n_classes = 1
+
+# tf Graph input
+x = tf.placeholder("float", [None, 18])
+y = tf.placeholder("float", [None])
+
+# Create model
+def multilayer_perceptron(x, weights, biases):
+    # Hidden layer with RELU activation
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)
+
+    # Hidden layer with RELU activation
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+
+    # Hidden layer with RELU activation
+    layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])
+    layer_3 = tf.nn.relu(layer_3)
+
+    # Hidden layer with RELU activation
+    layer_4 = tf.add(tf.matmul(layer_3, weights['h4']), biases['b4'])
+    layer_4 = tf.nn.relu(layer_4)
+
+    # Output layer with linear activation
+    out_layer = tf.matmul(layer_4, weights['out']) + biases['out']
+    return out_layer
+
+# Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1], 0, 0.1)),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2], 0, 0.1)),
+    'h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3], 0, 0.1)),
+    'h4': tf.Variable(tf.random_normal([n_hidden_3, n_hidden_4], 0, 0.1)),
+    'out': tf.Variable(tf.random_normal([n_hidden_4, n_classes], 0, 0.1))
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1], 0, 0.1)),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2], 0, 0.1)),
+    'b3': tf.Variable(tf.random_normal([n_hidden_3], 0, 0.1)),
+    'b4': tf.Variable(tf.random_normal([n_hidden_4], 0, 0.1)),
+    'out': tf.Variable(tf.random_normal([n_classes], 0, 0.1))
+}
+
+# Construct model
+pred = multilayer_perceptron(x, weights, biases)
+pred = tf.transpose(pred)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.square(pred-y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    # Training cycle
+    for epoch in range(training_epochs):
+        avg_cost = 0.
+        total_batch = int(total_len/batch_size)
+        # Loop over all batches
+        for i in range(total_batch-1):
+            batch_x = X_train[i*batch_size:(i+1)*batch_size]
+            batch_y = Y_train[i*batch_size:(i+1)*batch_size]
+            # Run optimization op (backprop) and cost op (to get loss value)
+            _, c, p = sess.run([optimizer, cost, pred], feed_dict={x: batch_x,
+                                                          y: batch_y})
+            # Compute average loss
+            avg_cost += c / total_batch
+
+        # sample prediction
+        label_value = batch_y
+        estimate = p
+
+        # Display logs per epoch step
+        if epoch % display_step == 0:
+            print ("Epoch:", '%04d' % (epoch+1), "cost=", \
+                "{:.9f}".format(avg_cost))
+            print ("[*]----------------------------")
+            for i in range(15):
+                print ("label value:", label_value[i], \
+                    "estimated value:", estimate[0][i])
+            print ("[*]============================")
+
+    print ("Optimization Finished!")
+
+    predicted_vals = sess.run(pred, feed_dict={x: X_test})
+    error = np.square(p-Y_test)*offset
+    print("error:")
+    print(np.sqrt(sum(error)/len(y_test)))
+
+
+    predicted_vals = scipy.ndimage.gaussian_filter(predicted_vals,5)
+    Y_train = scipy.ndimage.gaussian_filter(Y_train[-365:],5)
+    plt.plot(predicted_vals[0], label='predicted output',color="red")
+    plt.plot(Y_train, label='real output',color="blue")
+    plt.legend()
+    plt.xlabel("time (days)")
+    plt.ylabel("solar panel output (kWh)")
+    plt.title("Neural network predicted vs real output of one year")
+    plt.show()
