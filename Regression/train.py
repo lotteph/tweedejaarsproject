512f4ddb4114bea125f846e3149bf175a604d56a
basvdbrink1998 <basvdbrink1998@gmail.com>
2018-06-25 14:40:50 +0200

    plotting multiple years

--- Regression/train.py
+++ Regression/train.py
@@ -22,9 +22,9 @@ def make_csv(solar, weather):
     new["tilt"] = solar["Tilt"]
     return np.array(new)
 
-years = ["2009","2010","2011","2012","2013","2014","2015","2016","2017","2018"]
-postal_code = "2134"
-#years = ["2015","2017","2016"]
+postal_code = "2201"
+#years = ["2009","2010","2011","2012","2013","2014","2015","2016","2017"]
+years = ["2013","2014","2015","2016","2017"]
 
 W = pd.read_csv("../data/"+postal_code+ "_" + years[0] + "_W.csv")
 SP = pd.read_csv("../data/"+postal_code+ "_" + years[0] + "_S.csv")
@@ -50,32 +50,23 @@ W["visibility"] = 0
 # W["latitude"] = 0
 W = W.values
 
-train_size = len(results)-365*1
-
-x_train = W[:train_size,:]
-x_test =  W[train_size:,:]
-y_train = results[:train_size]
-y_test = results[train_size:]
-
-offset = SP["Number_of_panels"].values[-1]*SP["Max_power"].values[-1]
-
-def ridge_regression(par):
+def ridge_regression(x_train,y_train,x_test,y_test,offset,par):
     alpha = par[0]
     regr = linear_model.Ridge(alpha,solver="svd")
     regr.fit(x_train,y_train)
     ridge_pred = regr.predict(x_test)
-    pre = scipy.ndimage.gaussian_filter(ridge_pred,5)
-    plt.plot(pre,label='predicted output',color="red")
-    real = scipy.ndimage.gaussian_filter(y_test,5)
-    plt.plot(real,label='real output',color="blue")
-    plt.legend()
-    plt.xlabel("time (days)")
-    plt.ylabel("solar panel output (kWh)")
-    plt.title("ridge predicted vs real output of 2017")
-    plt.show()
+    # pre = scipy.ndimage.gaussian_filter(ridge_pred,5)
+    # plt.plot(pre,label='predicted output',color="red")
+    # real = scipy.ndimage.gaussian_filter(y_test,5)
+    # plt.plot(real,label='real output',color="blue")
+    # plt.legend()
+    # plt.xlabel("time (days)")
+    # plt.ylabel("solar panel output (kWh)")
+    # plt.title("ridge predicted vs real output of 2017")
+    # plt.show()
     return np.sqrt(np.sum(np.square(ridge_pred-y_test)))/len(y_test)*offset
 
-def Bayes_regression(par):
+def Bayes_regression(x_train,y_train,x_test,y_test,offset,par):
     alpha_1 = par[0]
     alpha_2 = par[1]
     lambda_1 = par[2]
@@ -83,41 +74,41 @@ def Bayes_regression(par):
     Bay = linear_model.BayesianRidge(alpha_1=alpha_1,alpha_2=alpha_2,lambda_1=lambda_1,lambda_2=lambda_2)
     Bay.fit(x_train,y_train)
     Bay_pred = Bay.predict(x_test)
-    pre = scipy.ndimage.gaussian_filter(Bay_pred,5)
-    plt.plot(pre,label='predicted output',color="red")
-    real = scipy.ndimage.gaussian_filter(y_test,5)
-    plt.plot(real,label='real output',color="blue")
-    plt.legend()
-    plt.xlabel("time (days)")
-    plt.ylabel("solar panel output (kWh)")
-    plt.title("bayes predicted vs real output of 2017")
-    plt.show()
+    # pre = scipy.ndimage.gaussian_filter(Bay_pred,5)
+    # plt.plot(pre,label='predicted output',color="red")
+    # real = scipy.ndimage.gaussian_filter(y_test,5)
+    # plt.plot(real,label='real output',color="blue")
+    # plt.legend()
+    # plt.xlabel("time (days)")
+    # plt.ylabel("solar panel output (kWh)")
+    # plt.title("bayes predicted vs real output of 2017")
+    # plt.show()
     return np.sqrt(np.sum(np.square(Bay_pred-y_test)))/len(y_test)*offset
 
 
-def decision_tree():
+def decision_tree(x_train,y_train,x_test,y_test,offset):
     dec = DecisionTreeRegressor(min_samples_split=0.1, presort=True)
     dec.fit(x_train, y_train)
     pred = dec.predict(x_test)
-    pre = scipy.ndimage.gaussian_filter(pred,5)
-    plt.plot(pre,label='predicted output',color="red")
-    real = scipy.ndimage.gaussian_filter(y_test,5)
-    plt.plot(real,label='real output',color="blue")
-    plt.legend()
-    plt.xlabel("time (days)")
-    plt.ylabel("solar panel output (kWh)")
-    plt.title("decision tree predicted vs real output of 2017")
-    plt.show()
+    # pre = scipy.ndimage.gaussian_filter(pred,5)
+    # plt.plot(pre,label='predicted output',color="red")
+    # real = scipy.ndimage.gaussian_filter(y_test,5)
+    # plt.plot(real,label='real output',color="blue")
+    # plt.legend()
+    # plt.xlabel("time (days)")
+    # plt.ylabel("solar panel output (kWh)")
+    # plt.title("decision tree predicted vs real output of 2017")
+    # plt.show()
     return np.sqrt(np.sum(np.square(pred-y_test)))/len(y_test)*offset
 
-def k_nearest(par):
-    neighbors = par[0]
+def k_nearest(x_train,y_train,x_test,y_test,offset,par):
+    neighbors = par
     neigh = KNeighborsRegressor(n_neighbors=int(neighbors))
     neigh.fit(x_train, y_train)
     neigh_pred = neigh.predict(x_test)
     return np.sqrt(np.sum(np.square(neigh_pred-y_test)))/len(y_test)*offset
 
-def kn_opt(iterations):
+def kn_opt(x_train,y_train,x_test,y_test,offset,iterations):
     best = 999999999999999999
     par = 1
     for i in range(1, iterations):
@@ -127,8 +118,33 @@ def kn_opt(iterations):
             par = [i]
     return(best, par)
 
-print("base: ", np.sqrt(np.sum(np.square(np.mean(y_train)-y_test)))/len(y_test)*offset)
-print("ridge: ",ridge_regression([-5]))
-print("new bayes: ",Bayes_regression([-3.63600029e-04,  2.33234414e-03,  5.52569969e-02, -4.99181236e-01]))
-# print("decision tree:", decision_tree())
-# print("KNN: ",kn_opt(5)[0])
+def multiple_years(years,N,W,results):
+    bas = np.zeros(len(years))
+    rid = np.zeros(len(years))
+    bay = np.zeros(len(years))
+    dec = np.zeros(len(years))
+    knn = np.zeros(len(years))
+    for i in range(0,N):
+        #W, results = shuffle(W, results)
+        for j in range(0,len(years)):
+            new_W = W[:365*(j+2)]
+            new_results = results[:365*(j+2)]
+            train_size = len(new_results)-365*1
+            x_train = new_W[:train_size,:]
+            x_test =  new_W[train_size:,:]
+            y_train = new_results[:train_size]
+            y_test = new_results[train_size:]
+            offset = SP["Number_of_panels"].values[-1]*SP["Max_power"].values[-1]
+            bas[j] += np.sqrt(np.sum(np.square(np.mean(y_train)-y_test)))/len(y_test)*offset
+            rid[j] += ridge_regression(x_train,y_train,x_test,y_test,offset,[-5])
+            bay[j] += Bayes_regression(x_train,y_train,x_test,y_test,offset,[-3.63600029e-04,  2.33234414e-03,  5.52569969e-02, -4.99181236e-01])
+            dec[j] += decision_tree(x_train,y_train,x_test,y_test,offset)
+            knn[j] += k_nearest(x_train,y_train,x_test,y_test,offset,5)
+    plt.plot(bas,label='base')
+    plt.plot(rid,label='ridge')
+    plt.plot(bay,label='bayes')
+    plt.plot(dec,label='decision tree')
+    plt.legend()
+    plt.show()
+
+multiple_years(years,100,W,results)
